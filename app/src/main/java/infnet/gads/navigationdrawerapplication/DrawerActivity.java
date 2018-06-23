dc619d8d063700b8aa972199eeee840cf85a4f2a
Jao Lima <jpfaria19@hotmail.com>
2018-06-23 02:10:18 -0300

    Init commit; Navigation Drawer done

---
+++ app/src/main/java/infnet/gads/navigationdrawerapplication/DrawerActivity.java
@@ -0,0 +1,137 @@
+package infnet.gads.navigationdrawerapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class DrawerActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        //int id = item.getItemId();
+
+        switch (item.getItemId()){
+            case R.id.nav_personInformation: {
+                Toast.makeText(this, "Informações Pessoais", Toast.LENGTH_LONG).show();
+                Intent personInformation = new Intent(DrawerActivity.this, MainActivity.class);
+                startActivity(personInformation);
+                break;
+            }
+            case R.id.nav_schoolEducation: {
+                Toast.makeText(this, "Formação", Toast.LENGTH_LONG).show();
+                Intent schoolEducation = new Intent(DrawerActivity.this, SchoolEducationActivity.class);
+                startActivity(schoolEducation);
+                break;
+            }
+            case R.id.nav_workExperiences: {
+                Toast.makeText(this, "Experiência Profissional", Toast.LENGTH_LONG).show();
+                Intent workExperiences = new Intent(DrawerActivity.this, WorkExperiencesActivity.class);
+                startActivity(workExperiences);
+                break;
+            }
+            case R.id.nav_courses: {
+                Toast.makeText(this, "Cursos", Toast.LENGTH_LONG).show();
+                Intent courses = new Intent(DrawerActivity.this, CoursesActivity.class);
+                startActivity(courses);
+                break;
+            }
+            case R.id.nav_publications: {
+                Toast.makeText(this, "Publicações", Toast.LENGTH_LONG).show();
+                Intent publications = new Intent(DrawerActivity.this, PublicationsActivity.class);
+                startActivity(publications);
+                break;
+            }
+
+        }
+
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/infnet/gads/navigationdrawerapplication/WorkExperiencesActivity.java
@@ -0,0 +1,13 @@
+package infnet.gads.navigationdrawerapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkExperiencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_experiences);
+    }
+}
